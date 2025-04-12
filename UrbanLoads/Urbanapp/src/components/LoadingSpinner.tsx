1851e2d31bb108ebd3035de94c768961979ccd34
Suman Adhithya <143721627+SumanAdhithya30@users.noreply.github.com>
2025-04-12 09:23:24 +0530

    Delete UrbanLoads directory

--- UrbanLoads/Urbanapp/src/components/LoadingSpinner.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const LoadingSpinner = () => {
-  return (
-    <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-blue-600"></div>
-  );
-};
-
-export default LoadingSpinner;
